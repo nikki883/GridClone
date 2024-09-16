1ab4b2931eddd825b5f76eab334a3e9e96fc0782
nikki dhakad <nikkidhakad224@gmail.com>
2024-09-16 20:48:21 +0530

    changes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let lightMode = document.querySelector(".lightMode")
 let darkMode = document.querySelector(".darkMode")
+let h3 = document.getElementsByTagName("h3")
 darkMode.addEventListener("click", () => {
     document.body.style.backgroundColor = 'Black'
     document.body.style.color = 'white'
-
+h3.style.color="white"
     console.log("button clicked")
 })
 lightMode.addEventListener("click", () => {
